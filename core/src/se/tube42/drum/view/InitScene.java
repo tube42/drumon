c5256045a8dc1556685b331220e06f18c49647db
Tubeman, The <tube42m@gmail.com>
2020-07-12 12:10:00 +0000

    Better load screen with a progress bar

--- core/src/se/tube42/drum/view/InitScene.java
+++ core/src/se/tube42/drum/view/InitScene.java
@@ -13,40 +13,53 @@ import se.tube42.drum.logic.*;
 
 import static se.tube42.drum.data.Constants.*;
 
+import java.util.HashMap;
+
 public class InitScene extends Scene implements Runnable {
 
-	private BaseText text;
-	private SpriteItem img;
+	private SpriteItem img, bar;
+	private int samples_total, samples_loaded;
 
 	public InitScene() {
 		super("loading");
 
+		samples_total = samples_loaded = 0;
+		for(int i = 0; i < VOICES; i++)
+			samples_total += SAMPLES[i].length;
+
 		ServiceProvider.setColorItem(0xFFFFFF, World.bgc, 0f, 1f, 0f); // light-gray background so we can see things
 		load_first_assets();
-
-		text = new BaseText(World.font2);
-		text.setAlignment(-0.5f, 0.5f);
-		text.setText("loading ...");
-		text.setColor(0x000000);
 
 		img = new SpriteItem(World.tex_icons, ICON_HELP);
 		img.setScale(2);
 
+		bar = new SpriteItem(World.tex_rect, 0);
+		bar.setColor(COLOR_PADS);
+
 		Layer l = getLayer(0);
-		l.add(text, img);
+		l.add(bar, img);
 
 		Thread t = new Thread(this);
 		t.start();
+
 	}
 
 	public void resize(int w, int h) {
 		final int size = World.size_button;
 		final int gap = size / 2;
 
-		img.setPosition((w - img.getW()) / 2, h / 2);
-		text.setPosition(w / 2, img.getY() - 1.5f * img.getH());
+		img.setPosition((w - img.getW()) / 2, h / 2 + img.getH() );
+		bar.setPosition(gap, h / 2 - size - gap);
+		bar.setSize(0, size);
+		bar.x2 = w - 2 * gap;
+		update_bar();
 	}
 
+	// this updates the load-bar which shows how much of the samples have been loaded
+	private void update_bar()
+	{
+		bar.setSize( bar.x2 * samples_loaded / samples_total, bar.getH());
+	}
 	// ----------------------------------------------------------
 
 	private void load_first_assets() {
@@ -75,13 +88,25 @@ public class InitScene extends Scene implements Runnable {
 	public void run() {
 
 		try {
+			// load samples, use a hashmap to avoid duplicates
+			HashMap<String, float []> samples = new HashMap<String, float []>();
 			for (int i = 0; i < VOICES; i++) {
 				img.setIndex(ICON_KICK + i);
 
+				for (int j = 0; j < SAMPLES[i].length; j++) {
+					if(! samples.containsKey(SAMPLES[i][j]))
+						samples.put(SAMPLES[i][j],  ServiceProvider.loadSample("samples/" + SAMPLES[i][j], World.freq));
+					samples_loaded++;
+					update_bar();
+				}
+			}
+
+			// build the sample array
+			for (int i = 0; i < VOICES; i++) {
 				final int vcount = SAMPLES[i].length;
 				final float[][] data = new float[vcount][];
 				for (int j = 0; j < vcount; j++)
-					data[j] = ServiceProvider.loadSample("samples/" + SAMPLES[i][j], World.freq);
+					data[j] = samples.get(SAMPLES[i][j]);
 				World.sounds[i] = new Sample(data);
 			}
 
